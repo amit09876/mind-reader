d8a5e3d6eb4f33e54839fe848da700a0b96de0b1
Amit Brackler <brackler100@gmail.com>
2019-01-21 10:30:32 +0530

    Almost done

--- app/src/main/java/net/simplifiedcoding/mindreader/MindReadingActivity.java
+++ app/src/main/java/net/simplifiedcoding/mindreader/MindReadingActivity.java
@@ -3,11 +3,13 @@ package net.simplifiedcoding.mindreader;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MindReadingActivity extends AppCompatActivity {
 
+    private int count = 0;
     private TextView textViewName, textViewTask;
     private Button buttonContinue;
 
@@ -26,5 +28,25 @@ public class MindReadingActivity extends AppCompatActivity {
         //XX -> data type
         String name = intent.getStringExtra("key_name");
         textViewName.setText(name);
+
+        buttonContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               count++;
+
+               if(count == 1){
+                   textViewTask.setText("Multiply the number with 2");
+               }else if(count == 2){
+                   textViewTask.setText("Add 8 to the result");
+               }else if(count == 3){
+                   textViewTask.setText("Divide the result by 2");
+               }else if(count == 4){
+                   textViewTask.setText("Now subtract the first number that you thought with the result");
+                   buttonContinue.setText("See the Magic");
+               }else{
+
+               }
+            }
+        });
     }
 }
